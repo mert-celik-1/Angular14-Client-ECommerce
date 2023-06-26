adcf3dc4dd923984fb3572237c95bb7896d82d7b
mert-celik-1 <mertcelik2399@gmail.com>
2023-06-26 18:40:26 +0300

    order

--- src/app/admin/components/dashboard/dashboard.component.ts
+++ src/app/admin/components/dashboard/dashboard.component.ts
@@ -1,11 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
-import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
-import { HubUrls } from 'src/app/constants/hub-url';
-import { ReceiveFunctions } from 'src/app/constants/receive-functions';
-
+import { BaseComponent } from 'src/app/base/base.component';
 import { AlertifyService, MessageType, Position } from 'src/app/services/admin/alertify.service';
-import { SignalRService } from 'src/app/services/common/signalr.service';
+import { ReceiveFunctions } from '../../../constants/receive-functions';
+import { SignalRService } from '../../../services/common/signalr.service';
+import { HubUrls } from 'src/app/constants/hub-url';
 
 @Component({
   selector: 'app-dashboard',
@@ -16,7 +15,9 @@ export class DashboardComponent extends BaseComponent implements OnInit {
 
   constructor(private alertify: AlertifyService, spinner: NgxSpinnerService, private signalRService: SignalRService) {
     super(spinner)
+    signalRService.start(HubUrls.OrderHub)
     signalRService.start(HubUrls.ProductHub)
+
   }
 
   ngOnInit(): void {
@@ -24,6 +25,12 @@ export class DashboardComponent extends BaseComponent implements OnInit {
       this.alertify.message(message, {
         messageType: MessageType.Notify,
         position: Position.TopRight
+      })
+    });
+    this.signalRService.on(ReceiveFunctions.OrderAddedMessageReceiveFunction, message => {
+      this.alertify.message(message, {
+        messageType: MessageType.Notify,
+        position: Position.TopCenter
       })
     });
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -45,6 +45,8 @@ import { DynamicLoadComponentDirective } from './directives/common/dynamic-load-
   ],
   providers: [
     { provide: "baseUrl", useValue: "https://localhost:7087/api", multi: true },
+    { provide: "baseSignalRUrl", useValue: "https://localhost:7087/", multi: true },
+
     {
       provide: "SocialAuthServiceConfig",
       useValue: {
